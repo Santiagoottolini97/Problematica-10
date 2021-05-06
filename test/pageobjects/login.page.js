2f318b617a3ea3ff39f32b3b212f47107af77e21
santiago <santiago.ottolini97@hotmail.com>
2021-05-06 16:23:00 -0300

    P14: Order code

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,54 +1,69 @@
 class LoginPage {
-    get inputEmail() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>div[class="containerEmail"]>input[id="email"]'
-        );
-    }
-    get inputPassword() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>div[class="containerPassword"]>input[id="password"]'
-        );
-    }
-    get btnSubmit() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>button[type="button"]'
-        );
-    }
-    get btnRegister() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>div[class="containerLinkRegister"]>a[class="linkRegister"]'
-        );
-    }
-    get errorEmail() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>div[class="containerEmail"]>div[id="errorEmail"]'
-        ).getText();
-    }
-    get errorPassword() {
-        return $(
-            'div[class="containerLogin"]>form[class="loginForm"]>div[class="containerPassword"]>div[id="errorPassword"]'
-        ).getText();
-    }
+  get inputEmail() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       div[class="containerEmail"]>
+       input[id="email"]`
+    );
+  }
+  get inputPassword() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       div[class="containerPassword"]>
+       input[id="password"]`
+    );
+  }
+  get btnSubmit() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       button[type="button"]`
+    );
+  }
+  get btnRegister() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       div[class="containerLinkRegister"]>
+       a[class="linkRegister"]`
+    );
+  }
+  get errorEmail() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       div[class="containerEmail"]>
+       div[id="errorEmail"]`
+    ).getText();
+  }
+  get errorPassword() {
+    return $(
+      `div[class="containerLogin"]>
+       form[class="loginForm"]>
+       div[class="containerPassword"]>
+       div[id="errorPassword"]`
+    ).getText();
+  }
 
-    buttonRegister() {
-        this.btnRegister.click();
-    }
-    testPassword(email, password) {
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.btnSubmit.click();
-        return this.errorPassword;
-    }
-    testEmail(email, password) {
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.btnSubmit.click();
-        return this.errorEmail;
-    }
+  buttonRegister() {
+    this.btnRegister.click();
+  }
+
+  testPassword(email, password) {
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.btnSubmit.click();
+    return this.errorPassword;
+  }
+  
+  testEmail(email, password) {
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.btnSubmit.click();
+    return this.errorEmail;
+  }
 }
 
 module.exports = new LoginPage();
--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -1,127 +1,123 @@
 class RegisterPage {
-    get inputName() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerName"]>input[id="name"]'
-        );
-    }
-    get inputEmail() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerEmail"]>input[id="email"]'
-        );
-    }
-    get inputPassword() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerPassword"]>input[id="password"]'
-        );
-    }
-    get inputRepeatPassword() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerPassword"]>input[id="rPassword"]'
-        );
-    }
-    get btnSubmit() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>button[type="button"]'
-        );
-    }
-    get btnLogin() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerLinkLogin"]>a[class="linkLogin"]'
-        );
-    }
-    get errorEmail() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerEmail"]>div[id="errorEmail"]'
-        ).getText();
-    }
-    get errorPassword() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerPassword"]>div[id="errorPassword"]'
-        ).getText();
-    }
-    get errorName() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerName"]>div[id="errorName"]'
-        ).getText();
-    }
-    get errorRepeatPassword() {
-        return $(
-            'div[class="containerRegister"]>form[class="registerForm"]>div[class="containerPassword"]>div[id="errorPasswordR"]'
-        ).getText();
-    }
+  get inputName() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerName"]>
+       input[id="name"]`
+    );
+  }
+  get inputEmail() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerEmail"]>
+       input[id="email"]`
+    );
+  }
+  get inputPassword() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerPassword"]>
+       input[id="password"]`
+    );
+  }
+  get inputRepeatPassword() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerPassword"]>
+       input[id="rPassword"]`
+    );
+  }
+  get btnSubmit() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       button[type="button"]`
+    );
+  }
+  get btnLogin() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerLinkLogin"]>
+       a[class="linkLogin"]`
+    );
+  }
+  get errorEmail() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerEmail"]>
+       div[id="errorEmail"]`
+    ).getText();
+  }
+  get errorPassword() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerPassword"]>
+       div[id="errorPassword"]`
+    ).getText();
+  }
+  get errorName() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerName"]>
+       div[id="errorName"]`
+    ).getText();
+  }
+  get errorRepeatPassword() {
+    return $(
+      `div[class="containerRegister"]>
+       form[class="registerForm"]>
+       div[class="containerPassword"]>
+       div[id="errorPasswordR"]`
+    ).getText();
+  }
 
-    buttonLogin() {
-        this.btnLogin.click();
-    }
+  buttonLogin() {
+    this.btnLogin.click();
+  }
 
-    testEmail(
-        name,
-        email,
-        password,
-        repeatPassword
-    ) {
-        this.inputName.setValue(name);
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.inputRepeatPassword.setValue(
-            repeatPassword
-        );
-        this.btnSubmit.click();
-        return this.errorEmail;
-    }
-    testName(
-        name,
-        email,
-        password,
-        repeatPassword
-    ) {
-        this.inputName.setValue(name);
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.inputRepeatPassword.setValue(
-            repeatPassword
-        );
-        this.btnSubmit.click();
-        return this.errorName;
-    }
-    testPassword(
-        name,
-        email,
-        password,
-        repeatPassword
-    ) {
-        this.inputName.setValue(name);
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.inputRepeatPassword.setValue(
-            repeatPassword
-        );
-        this.btnSubmit.click();
-        return this.errorPassword;
-    }
-    testRepeatPassword(
-        name,
-        email,
-        password,
-        repeatPassword
-    ) {
-        this.inputName.setValue(name);
-        this.inputEmail.setValue(email);
-        this.inputPassword.setValue(
-            password
-        );
-        this.inputRepeatPassword.setValue(
-            repeatPassword
-        );
-        this.btnSubmit.click();
-        return this.errorRepeatPassword;
-    }
+  testEmail(name, email, password, repeatPassword) {
+    this.inputName.setValue(name);
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.inputRepeatPassword.setValue(repeatPassword);
+    this.btnSubmit.click();
+    return this.errorEmail;
+  }
+
+  testName(name, email, password, repeatPassword) {
+    this.inputName.setValue(name);
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.inputRepeatPassword.setValue(repeatPassword);
+    this.btnSubmit.click();
+    return this.errorName;
+  }
+
+  testPassword(name, email, password, repeatPassword) {
+    this.inputName.setValue(name);
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.inputRepeatPassword.setValue(repeatPassword);
+    this.btnSubmit.click();
+    return this.errorPassword;
+  }
+
+  testRepeatPassword(name, email, password, repeatPassword) {
+    this.inputName.setValue(name);
+    this.inputEmail.setValue(email);
+    this.inputPassword.setValue(password);
+    this.inputRepeatPassword.setValue(repeatPassword);
+    this.btnSubmit.click();
+    return this.errorRepeatPassword;
+  }
 }
 
 module.exports = new RegisterPage();
